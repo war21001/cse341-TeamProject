ce73fb4fce79d1e4790fccae3ea0ed2d0df912a2
war21001 <83742731+war21001@users.noreply.github.com>
2022-01-07 18:32:45 -0700

    team activity

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,9 +3,35 @@
 const express = require('express');
 const router = express.Router();
 
+// For requirement 01 of TA02
+const userArray = ['Lisa', 'Stephanie', 'Austin', 'Michael'];
+
+// For requirement 02 of TA02
+router.post('/addUser', (req, res, next) => {
+  const newUser = req.body.newUser;
+
+  userArray.push(newUser);
+
+  res.redirect('/ta02/');
+});
+
+// For requirement 03 of TA02
+router.post('/removeUser', (req, res, next) => {
+  const remUser = req.body.remUser;
+
+  // Splice method removes from a const array
+  const index = userArray.indexOf(remUser);
+  if (index !== -1) {
+    userArray.splice(index, 1);
+  }
+
+  res.redirect('/ta02/');
+});
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
+    users: userArray,
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
