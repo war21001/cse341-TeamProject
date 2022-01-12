a351e6704a3c50b88b7bfcc4a72acb15162ac1c6
war21001 <83742731+war21001@users.noreply.github.com>
2022-01-11 20:45:29 -0700

    lesson3
    i didn't get the filter working yet

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,14 +1,34 @@
 //TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  res.render('pages/ta03', {
-    title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
-  });
+const http = require('https');
+let data = [];
+var url = 'https://byui-cse.github.io/cse341-course/lesson03/items.json';
+// // var options = {
+// //     host: 'www.random.org',
+// //     path: '/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new',
+// // };
+// }
+http.get(url, (res) => {
+    let str = '';
+    res.on('data', (chunk) => {
+        str += chunk;
+    });
+    res.on('end', () => {
+        data = JSON.parse(str);
+        // console.log(str);
+    });
+    res.on('error', (err) => {
+        console.log(err);
+    });
 });
-
+router.get('/', (req, res, next) => {
+    res.render('pages/ta03', {
+        title: 'Team Activity 03',
+        path: '/ta03', // For pug, EJS
+        activeTA03: true, // For HBS
+        contentCSS: true, // For HBS\
+        items: data,
+    });
+});
 module.exports = router;
